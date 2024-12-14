b2facbfc31f87c91b458c9cc3a3fdc2ab75a4cbb
martinsh <martins.ceske@gmail.com>
2024-12-14 14:01:52 +0200

    Initial commit

---
+++ src/main/java/com/moma/start_school_demo/Application.java
@@ -0,0 +1,13 @@
+package com.moma.start_school_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
